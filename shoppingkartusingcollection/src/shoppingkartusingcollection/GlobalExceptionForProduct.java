af4eaf09b4d09c842dfc18bd0afc6d7ae550336c
SOUMYADEEP DAS <109064628+amisoumyadeepdas@users.noreply.github.com>
2024-07-14 17:23:24 +0530

    Add files via upload

---
+++ shoppingkartusingcollection/src/shoppingkartusingcollection/GlobalExceptionForProduct.java
@@ -0,0 +1,10 @@
+package shoppingkartusingcollection;
+
+public class GlobalExceptionForProduct extends Exception {
+	
+	public GlobalExceptionForProduct (String message)
+	{
+		super(message);
+	}
+
+}
